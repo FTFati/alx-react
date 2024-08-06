72033289c5429f5e4dcbe60cafcaaca8da1f6adb
FTFati <133517391+FTFati@users.noreply.github.com>
2024-08-06 16:29:58 +0100

    Update courseActionCreators.test.js

--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
@@ -1,15 +1,48 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
-import { selectCourse, unSelectCourse } from "./courseActionCreators";
+import {
+  selectCourse,
+  unSelectCourse,
+  setCourses,
+  fetchCourses,
+} from "./courseActionCreators";
 
-describe("courseActionCreators tests", () => {
-    it("should return SELECT_COURSE action with right index", () => {
-        const expectedAction = { type: SELECT_COURSE, index: 1 };
+import {
+  SELECT_COURSE,
+  UNSELECT_COURSE,
+  FETCH_COURSE_SUCCESS,
+} from "./courseActionTypes";
 
-        expect(selectCourse(1)).toEqual(expectedAction);
+import fetchMock from "fetch-mock";
+
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe("action creators tests", function () {
+  it("selectCourse should return: { type: SELECT_COURSE, index: 1 }", function () {
+    const result = selectCourse(1);
+
+    expect(result).toEqual({ type: SELECT_COURSE, index: 1 });
+  });
+  it("unSelectCourse should return: { type: UNSELECT_COURSE, index: 1 }", function () {
+    const result = unSelectCourse(1);
+
+    expect(result).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  });
+  it("unSelectCourse should return: { type: UNSELECT_COURSE, index: 1 }", function () {
+    const result = unSelectCourse(1);
+
+    expect(result).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  });
+  it("verify that the fetch is working correctly", function () {
+    const store = mockStore({});
+    fetchMock.restore();
+
+    fetchMock.get("./courses.json", "{}");
+
+    return store.dispatch(fetchCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual(setCourses({}));
     });
-
-    it("should return UNSELECT_COURSE action with right index", () => {
-        const expectedAction =  { type: UNSELECT_COURSE, index: 1 }
-        expect(unSelectCourse(1)).toEqual(expectedAction);
-    });
+  });
 });
